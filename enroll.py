1460bb64e82fac80635f4c556d0c56b8153510d0
yavichra <yavichra@gmail.com>
2018-08-15 00:40:29 +0300

    Major bug fixes_2

--- enroll.py
+++ enroll.py
@@ -1,6 +1,22 @@
 from tkinter import filedialog
-from tkinter import *
+from tkinter import Text, Scrollbar, Frame, Tk, Button, LEFT
 
+def quit_app():
+    global root
+    root.destroy()
+    
+def loadfile(): 
+    global textbox
+    filename = filedialog.askopenfile(filetypes = [('*.txt files', '.txt')])
+    if filename:
+        textbox.delete('1.0', 'end')
+        textbox.insert('1.0', open(filename.name, 'rt').read())
+
+def savefile():
+    global textbox
+    filename = filedialog.asksaveasfile(filetypes = [('*.txt files', '.txt')])
+    if filename:
+        open(filename.name, 'wt').write(textbox.get('1.0', 'end'))
 
 root = Tk()
 
@@ -19,31 +35,23 @@ textbox['yscrollcommand'] = scrollbar.set
 textbox.pack(side = 'left', fill = 'both', expand = 1)
 scrollbar.pack(side = 'right', fill = 'y')
 
-loadBtn = Button(panelFrame, text = 'Load')
-saveBtn = Button(panelFrame, text = 'Save')
-quitBtn = Button(panelFrame, text = 'Quit')
 
-def Quit(ev):
-    global root
-    root.destroy()
-    
-def LoadFile(ev): 
-    filename = filedialog.askopenfile(filetypes = [('*.txt files', '.txt')])
-    if filename:
-        textbox.delete('1.0', 'end')
-        textbox.insert('1.0', open(filename.name, 'rt').read())
 
-def SaveFile(ev):
-    filename = filedialog.asksaveasfile(filetypes = [('*.txt files', '.txt')])
-    if filename:
-        open(filename.name, 'wt').write(textbox.get('1.0', 'end'))
 
-loadBtn.bind("<Button-1>", LoadFile)
-saveBtn.bind("<Button-1>", SaveFile)
-quitBtn.bind("<Button-1>", Quit)
 
-loadBtn.place(x = 10, y = 10, width = 40, height = 40)
-saveBtn.place(x = 60, y = 10, width = 40, height = 40)
-quitBtn.place(x = 110, y = 10, width = 40, height = 40)
+loadBtn = Button(panelFrame, text = 'Load', command = loadfile)
+loadBtn.pack(side=LEFT, padx=(10,5), pady=5)
+saveBtn = Button(panelFrame, text = 'Save', command = savefile)
+saveBtn.pack(side=LEFT, padx=5, pady=5)
+quitBtn = Button(panelFrame, text = 'Quit', command = quit_app)
+quitBtn.pack(side=LEFT, padx=5, pady=5)
+# loadBtn.bind("<Button-1>", LoadFile)
+# saveBtn.bind("<Button-1>", SaveFile)
+# quitBtn.bind("<Button-1>", Quit)
+
+
+# loadBtn.place(x = 10, y = 10, width = 40, height = 40)
+# saveBtn.place(x = 60, y = 10, width = 40, height = 40)
+# quitBtn.place(x = 110, y = 10, width = 40, height = 40)
 
 root.mainloop()
